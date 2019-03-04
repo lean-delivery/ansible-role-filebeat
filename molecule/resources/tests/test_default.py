020d5841fca893970fed6e5aa73d3434cdf2fe9a
Timur Gadiev <Timur_Gadiev@epam.com>
2019-03-04 22:41:15 +0300

    Fix test
    Signed-off-by: Timur Gadiev <Timur_Gadiev@epam.com>

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -2,8 +2,10 @@ import os
 
 import testinfra.utils.ansible_runner
 
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
 
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
